596f9e1fb89690319b388fa0e62cf6cb903d77b4
Jawadban <jawadbanga@gmail.com>
2017-02-15 23:56:31 -0800

    following best practices of breaking components into smaller ones

--- app.js
+++ app.js
@@ -1,25 +1,15 @@
 var Bananas = () => <li>Bananas</li>
 
-var todos=['Learn React', 'Crush Recast.ly', 'Maybe sleep'];
-
-var todoList = todos.map((todo) => 
-	<li>{todo}</li>
-);
-  	
 var GroceryList = () => (
 	<div>
 		<div>
 			<ul>
 				<Bananas />
 				<li>Stawberries</li>
+				<div>{[<li>trump</li>,<li>bernie</li>]}</div>
 			</ul>
-		</div>
-		<div>
-			<ul>{todoList}</ul>
 		</div>
 	</div>
 );
-
-console.log(todos);
 
 ReactDOM.render(<GroceryList />, document.getElementById('app'));
--- todoList.js
+++ todoList.js
@@ -1,16 +1,26 @@
+
+// var todoEntery = (props) => (
+// 	//<li>{props}</li>
+// );
+
+// var TodoList = () => (
+// 		<ul>{ todos.map((todo) => <li>{todo}</li>)}</ul>
+// );
+
+// var todos=['Learn React', 'Crush Recast.ly', 'Maybe sleep'];
+
+// ReactDOM.render(<TodoList />, document.getElementById('todoApp'));
+
+
+
+var list=['Learn React', 'Crush Recast.ly', 'Maybe sleep'];
+
+var TodoEntery = (props) => <div>{ props.todos.map( (todo) => <li>{todo}</li> ) }</div>
+
 var TodoList = (props) => (
-	<div>
-		<ul>
-	    <li>{props.todos[0]}</li>
-	    <li>{props.todos[1]}</li>
-	    <li>{props.todos[2]}</li>
-		</ul>
-	</div>
+	<ul>
+		<TodoEntery todos={props.todos}/>
+	</ul>
 );
 
-// var todos=['Learn React', 'Crush Recast.ly', 'Maybe sleep']
- 
-// adding the list to the window global object
-window.TodoList = TodoList;
-//window.todos = todos;
-// ReactDOM.render(<todoList />, getElementById(''));
+ReactDOM.render(<TodoList todos={list}/>, document.getElementById('todoApp'));
