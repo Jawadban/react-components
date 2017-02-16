7594c285504f7f02573e0d8f30bff80c2df15e2c
Jawadban <jawadbanga@gmail.com>
2017-02-15 23:57:46 -0800

    cleaning up redundant code

--- todoList.js
+++ todoList.js
@@ -1,18 +1,4 @@
 
-// var todoEntery = (props) => (
-// 	//<li>{props}</li>
-// );
-
-// var TodoList = () => (
-// 		<ul>{ todos.map((todo) => <li>{todo}</li>)}</ul>
-// );
-
-// var todos=['Learn React', 'Crush Recast.ly', 'Maybe sleep'];
-
-// ReactDOM.render(<TodoList />, document.getElementById('todoApp'));
-
-
-
 var list=['Learn React', 'Crush Recast.ly', 'Maybe sleep'];
 
 var TodoEntery = (props) => <div>{ props.todos.map( (todo) => <li>{todo}</li> ) }</div>
